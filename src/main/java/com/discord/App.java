5ba44219dda8551e9cfcece4a39048a96a8c7b5f
Johnathan <jchan0867@gmail.com>
2024-05-30 00:40:31 +1000

    Changes

---
+++ src/main/java/com/discord/App.java
@@ -0,0 +1,26 @@
+package com.discord;
+
+
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws LoginException
+    {
+    	
+       JDA jda = JDABuilder.createDefault("ODYyMTk5NTg2NDE1MzEyODk2.YOU38A.jHJQFRyQ4_HtQoy1-iX1UFb8Eck").build();
+       jda.addEventListener(new bot());
+       jda.getPresence().setActivity(Activity.listening("$help in DMs	"));
+
+    }
+}
+
